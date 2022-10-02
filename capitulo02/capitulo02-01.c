0b81cb12d810fe17013c3aa4454d8d18215b2021
Thamires Falbo <111710331+thamiresfalbo@users.noreply.github.com>
2022-10-01 22:07:52 -0300

    Fist Commit.

---
+++ capitulo02/capitulo02-01.c
@@ -0,0 +1,15 @@
+/* Capítulo 02 - Exercício 1 */
+#include <math.h>
+#include <stdio.h>
+
+void volume_esfera(float raio) {
+  // M_PI já está definido como π na biblioteca math.h
+  float value = 4 * M_PI * pow(raio, 3) / 3; 
+  printf("%.2f", value);
+}
+
+int main(void) {
+  volume_esfera(5.00);
+  return 0;
+}
+
