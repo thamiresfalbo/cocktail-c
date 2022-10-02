a7a24a576d1ea8dd19a3098910bdbcd3af415e1a
Thamires Falbo <111710331+thamiresfalbo@users.noreply.github.com>
2022-10-01 21:37:23 -0300

    Fist Commit.

---
+++ capitulo02/capitulo02-04.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+//  Moedas: 1 gold coin
+//  100 gp -> 1 platinum
+//  10.000 gp -> 1 crystal coin (ou 100 platinum)
+
+typedef struct {
+  char* key;
+  int value;
+} item;
+
+void celulas(int valor) {
+  const item notas[] = { {"Cristal", 10000}, {"Platinum", 100}, {"Gold", 1}};
+  for (int i = 0; i < 3; ++i) {
+    printf("%s : %d \n", notas[i].key, (valor/notas[i].value));
+    valor %= notas[i].value;
+  }
+  printf("-------- \n");
+}
+
+int main() {
+  celulas(1250);
+  celulas(200);
+}
